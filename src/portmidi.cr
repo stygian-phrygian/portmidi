a2ca79c17cd056bc21cc37963da69d4a3d8d9d1f
Josh W <stygian-phrygian@users.noreply.github.com>
2017-07-31 02:34:59 -0400

    added more things to portmidi.cr, still unsure of what the overall API structure will be

--- src/portmidi.cr
+++ src/portmidi.cr
@@ -1,6 +1,65 @@
 require "./portmidi/*"
 
+lib C
+  fun printf(format : UInt8*, ...) : Int32
+end
+
 module PortMidi
+
+  def initialize()
+    LibPortMidi.initialize()
+  end
+
+  def terminate()
+    LibPortMidi.terminate()
+  end
+
+  # c String to crystal String
+  private def cs_to_s(p : LibC::Char*)
+    return "" unless p
+    s = String.build do |sb|
+      while p.value != 0
+        sb << p.value.chr
+        p = p + 1
+      end
+    end
+    s
+  end
+
+  private def get_midi_devices() : Array(MidiDevice)
+    devices = Array(MidiDevice)
+    deviceInfo : LibPortMidi::PmDeviceInfo
+    LibPortMidi.count_devices().times do |deviceID|
+      deviceInfo = LibPortMidi.get_device_info(deviceID).value
+      #TODO
+    end
+    return devices
+  end
+
+  struct MidiDevice
+    # stream
+    private def initialize
+    end
+    def self.open(deviceID : Int32)
+    end
+    def close
+    end
+    def write
+    end
+    def read
+    end
+    def listen(callback)
+    end
+  end
+
+  # struct PmDeviceInfo
+  #   structVersion : Int
+  #   interf        : LibC::Char*
+  #   name          : LibC::Char*
+  #   input         : Int
+  #   output        : Int
+  #   opened        : Int
+  # end
 end
 # testing
 ##
@@ -8,9 +67,9 @@ puts "The number of available midi devices is:"
 number_of_devices = LibPortMidi.count_devices()
 puts number_of_devices
 #
-
-#number_of_devices.times do |i|
-#    info = PortMidi.get_device_info(i).value
-#    puts info.name
-#end
+puts "the devices are: "
+number_of_devices.times do |i|
+    info = LibPortMidi.get_device_info(i).value
+    puts i.to_s + cs_to_s(info.name)
+end
 
