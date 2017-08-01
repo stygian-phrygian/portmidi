e2992000f31e98e8e8d69655e2d95cb327431c63
Josh W <stygian-phrygian@users.noreply.github.com>
2017-08-01 08:05:15 -0400

    reworked everything, still experimenting with API ideas

--- src/portmidi.cr
+++ src/portmidi.cr
@@ -1,75 +1,99 @@
 require "./portmidi/*"
 
-lib C
-  fun printf(format : UInt8*, ...) : Int32
-end
+class PortMidi
 
-module PortMidi
+  @@opened_midi_streams = Hash(Int32, Void*).new
 
-  def initialize()
-    LibPortMidi.initialize()
+  def self.start()
+    check_error(LibPortMidi.initialize)
+    #populate_midi_devices
   end
 
-  def terminate()
-    LibPortMidi.terminate()
+  def self.stop()
+    check_error LibPortMidi.terminate
+    #@@midi_devices.clear
   end
 
-  # c String to crystal String
-  private def cs_to_s(p : LibC::Char*)
-    return "" unless p
-    s = String.build do |sb|
-      while p.value != 0
-        sb << p.value.chr
-        p = p + 1
+  class PortMidiException < Exception
+  end
+
+  private def self.check_error(error : LibPortMidi::PmError)
+    unless error == LibPortMidi::PmError::PmNoError
+        raise PortMidiException.new String.new(LibPortMidi.get_error_text(error))
+    end
+  end
+
+  def self.get_all_midi_devices()
+    devices = Array(MidiDeviceInfo).new
+    device_info : LibPortMidi::PmDeviceInfo
+    LibPortMidi.count_devices().times do |device_id|
+      device_info = LibPortMidi.get_device_info(device_id).value
+      devices << MidiDeviceInfo.new(device_id, device_info) if device_info != 0
+    end
+    devices
+  end
+
+  def self.get_midi_inputs()
+      devices = get_all_midi_devices.select {|d| d.input}
+  end
+
+  def self.get_midi_outputs()
+      devices = get_all_midi_devices.select {|d| d.output}
+  end
+
+
+  class MidiDeviceInfo
+      # this class is the same as the PmDeviceInfo struct
+      # with the addition of the device id for added convenience
+
+      # these need type specification (else crystal refuses to compile)
+      @input : Bool
+      @output : Bool
+      getter :input, :output, :opened, :name, :device_id
+
+    def initialize(device_id : Int32, device_info : LibPortMidi::PmDeviceInfo)
+      @device_id = device_id
+      @name = String.new(device_info.name)
+      @input = device_info.input != 0
+      @output = device_info.output != 0
+      @opened = device_info.opened != 0
+    end
+
+    def open()
+      unless @opened
+        if @input
+          LibPortMidi.open_input(@stream.address, @device_id, 0, 512, 0, 0)
+        else
+          LibPortMidi.open_output(@stream.address, @device_id, 0, 512, 0, 0, 0)
+        end
+        @opened = true
       end
     end
-    s
-  end
 
-  private def get_midi_devices() : Array(MidiDevice)
-    devices = Array(MidiDevice)
-    deviceInfo : LibPortMidi::PmDeviceInfo
-    LibPortMidi.count_devices().times do |deviceID|
-      deviceInfo = LibPortMidi.get_device_info(deviceID).value
-      #TODO
-    end
-    return devices
-  end
-
-  struct MidiDevice
-    # stream
-    private def initialize
-    end
-    def self.open(deviceID : Int32)
-    end
     def close
+      if @opened
+        LibPortMidi.close(@stream)
+        @opened = false
+      end
     end
+
     def write
     end
+
     def read
     end
+
     def listen(callback)
     end
+
   end
 
-  # struct PmDeviceInfo
-  #   structVersion : Int
-  #   interf        : LibC::Char*
-  #   name          : LibC::Char*
-  #   input         : Int
-  #   output        : Int
-  #   opened        : Int
-  # end
-end
-# testing
-##
-puts "The number of available midi devices is:"
-number_of_devices = LibPortMidi.count_devices()
-puts number_of_devices
-#
-puts "the devices are: "
-number_of_devices.times do |i|
-    info = LibPortMidi.get_device_info(i).value
-    puts i.to_s + cs_to_s(info.name)
 end
 
+
+PortMidi.start
+#PortMidi.get_midi_inputs.each {|i| p i}
+p PortMidi.get_all_midi_devices.select {|d| d.name.match /2/}
+PortMidi.stop
+#PortMidi::devices.each {|d| p d}
+#PortMidi::terminate
