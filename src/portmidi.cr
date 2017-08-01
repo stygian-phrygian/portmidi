0cee5d46bc8dbfcea66e2c6b4668601d209f715f
Josh W <stygian-phrygian@users.noreply.github.com>
2017-08-01 17:58:01 -0400

    open and close work now

--- src/portmidi.cr
+++ src/portmidi.cr
@@ -1,19 +1,16 @@
 require "./portmidi/*"
 
 module PortMidi
-
   extend self
 
-  @@opened_midi_streams = Hash(Int32, LibPortMidi::PortMidiStream*).new
-
-  def start()
+  def start
     check_error(LibPortMidi.initialize)
-    #populate_midi_devices
+    # populate_midi_devices
   end
 
-  def stop()
+  def stop
     check_error LibPortMidi.terminate
-    #@@midi_devices.clear
+    # @@midi_devices.clear
   end
 
   class PortMidiException < Exception
@@ -21,28 +18,31 @@ module PortMidi
 
   private def check_error(error : LibPortMidi::PmError)
     unless error == LibPortMidi::PmError::PmNoError
-        raise PortMidiException.new String.new(LibPortMidi.get_error_text(error))
+      raise PortMidiException.new String.new(LibPortMidi.get_error_text(error))
     end
   end
 
-  def get_all_midi_devices()
-    devices = Array(MidiDeviceInfo).new
+  def get_all_midi_devices
+    devices = Array(MidiDevice).new
     device_info : LibPortMidi::PmDeviceInfo
-    LibPortMidi.count_devices().times do |device_id|
+    LibPortMidi.count_devices.times do |device_id|
       device_info = LibPortMidi.get_device_info(device_id).value
-      devices << MidiDeviceInfo.new(device_id, device_info) if device_info != 0
+      devices << MidiDevice.new(device_id, device_info) if device_info != 0
     end
     devices
   end
 
-  class MidiDeviceInfo
-      # this class is the same as the PmDeviceInfo struct
-      # with the addition of the device id for added convenience
+  private class MidiDevice
+    @@opened_streams = Hash(Int32, LibPortMidi::PortMidiStream*).new
 
-      # these need type specification (else crystal refuses to compile)
-      @input : Bool
-      @output : Bool
-      getter :input, :output, :opened, :name, :device_id
+    # this class is the same as the PmDeviceInfo struct
+    # with the addition of the device id for added convenience
+
+    # these need type specification (else crystal refuses to compile)
+    @input : Bool
+    @output : Bool
+
+    getter :input, :output, :opened, :name, :device_id
 
     def initialize(device_id : Int32, device_info : LibPortMidi::PmDeviceInfo)
       @device_id = device_id
@@ -50,27 +50,31 @@ module PortMidi
       @input = device_info.input != 0
       @output = device_info.output != 0
       @opened = device_info.opened != 0
+      @stream = uninitialized LibPortMidi::PortMidiStream*
     end
 
-    def open()
-      unless @opened
+    def open
+      unless @@opened_streams[@device_id]?
         if @input
-          LibPortMidi.open_input(@stream.address, @device_id, 0, 512, 0, 0)
+          LibPortMidi.open_input(out @stream, @device_id, nil, 512, nil, nil)
         else
-          LibPortMidi.open_output(@stream.address, @device_id, 0, 512, 0, 0, 0)
+          LibPortMidi.open_output(out @stream, @device_id, nil, 512, nil, nil, 0)
         end
         @opened = true
+        @@opened_streams[@device_id] = @stream
       end
     end
 
     def close
-      if @opened
+      if @@opened_streams[@device_id]?
         LibPortMidi.close(@stream)
         @opened = false
+        @@opened_streams.delete @device_id
+        @stream.value = nil
       end
     end
 
-    def write
+    def write(bytes)
     end
 
     def read
@@ -78,15 +82,19 @@ module PortMidi
 
     def listen(callback)
     end
-
   end
-
 end
 
-
 PortMidi.start
-#PortMidi.get_midi_inputs.each {|i| p i}
-p PortMidi.get_all_midi_devices.select {|d| d.name.match /2/}
+d_in = PortMidi.get_all_midi_devices.select { |d| d.input }[0]
+d_out = PortMidi.get_all_midi_devices.select { |d| d.output }[0]
+d_in.open
+d_out.open
+p d_in
+p d_out
+d_in.close
+d_out.close
+
+# p PortMidi.get_all_midi_devices.select {|d| d.input }
+# p PortMidi.get_all_midi_devices.select {|d| d.name.match /2/}
 PortMidi.stop
-#PortMidi::devices.each {|d| p d}
-#PortMidi::terminate
