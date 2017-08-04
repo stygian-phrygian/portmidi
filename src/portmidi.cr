325b71964e931576e64925c18bb469b48237a65c
Josh W <stygian-phrygian@users.noreply.github.com>
2017-08-03 22:52:56 -0400

    completely reworked api, (simplifying it)

--- src/portmidi.cr
+++ src/portmidi.cr
@@ -11,80 +11,24 @@ module PortMidi
     check_error LibPortMidi.terminate
   end
 
-  class PortMidiException < Exception
+  def get_all_midi_device_info
+    Array(MidiDeviceInfo).new(LibPortMidi.count_devices) do |device_id|
+      MidiDeviceInfo.new device_id
+    end
   end
 
-  private def check_error(error : LibPortMidi::PmError)
+  def check_error(error : LibPortMidi::PmError)
     unless error == LibPortMidi::PmError::PmNoError
       raise PortMidiException.new String.new(LibPortMidi.get_error_text(error))
     end
   end
 
-  # this returns the PmDeviceInfo struct from the PortMidi c LibPortMidi
-  # the enduser needn't use use this ever (they should call get_all_midi_devices)
-  private def get_midi_device_info(device_id)
-    device_info_p = LibPortMidi.get_device_info(device_id)
-    raise PortMidiException.new("Invalid device id, cannot retrieve device info") unless device_info_p
-    device_info_p.value
-  end
+  private abstract class MidiStream
+    getter :device_id, :stream
 
-  def get_all_midi_devices
-    Array(MidiDevice).new(LibPortMidi.count_devices) do |device_id|
-      MidiDevice.new device_id, get_midi_device_info(device_id)
-    end
-  end
-
-  # this class is the same as the PmDeviceInfo struct
-  # with the addition of the device id for added convenience
-  private class MidiDevice
-    @@opened_streams = Hash(Int32, LibPortMidi::PortMidiStream*).new
-
-    # these need type specification (else crystal refuses to compile)
-    @input : Bool
-    @output : Bool
-
-    getter :input, :output, :name, :device_id
-
-    def initialize(device_id : Int32, device_info : LibPortMidi::PmDeviceInfo)
-      @device_id = device_id
-      @name = String.new(device_info.name)
-      @input = device_info.input != 0
-      @output = device_info.output != 0
-      @opened = device_info.opened != 0
+    def initialize(@device_id : Int32)
       @stream = uninitialized LibPortMidi::PortMidiStream*
-    end
-
-    # writting the opened getter so it can reflect updates accurately
-    def opened
-      device_info = get_midi_device_info @device_id
-      @opened = device_info.open != 0
-    end
-
-    private def check_error(error : LibPortMidi::PmError)
-      unless error == LibPortMidi::PmError::PmNoError
-        raise PortMidiException.new String.new(LibPortMidi.get_error_text(error))
-      end
-    end
-
-    def open
-      unless @@opened_streams[@device_id]?
-        if @input
-          check_error LibPortMidi.open_input(out @stream, @device_id, nil, 512, nil, nil)
-        else
-          check_error LibPortMidi.open_output(out @stream, @device_id, nil, 512, nil, nil, 0)
-        end
-        @opened = true
-        @@opened_streams[@device_id] = @stream
-      end
-    end
-
-    def close
-      if @@opened_streams[@device_id]?
-        check_error LibPortMidi.close(@stream)
-        @opened = false
-        @@opened_streams.delete @device_id
-        @stream.value = nil
-      end
+      # opening the input/output stream happens here in the subclasses
     end
 
     # this only writes "Channel Voice Messages" (not SysEx)
@@ -96,7 +40,7 @@ module PortMidi
         event.message = messages[i].to_i32
         event
       end
-      check_error LibPortMidi.write(@stream, buffer, buffer.size)
+      PortMidi.check_error LibPortMidi.write(@stream, buffer, buffer.size)
     end
 
     def read
@@ -104,24 +48,81 @@ module PortMidi
       # read() returns the number of events read
       # OR a negative integer (representing a PmError enum value)
       events_read = LibPortMidi.read(@stream, buffer, buffer.size)
-      check_error LibPortMidi::PmError.new(events_read) if events_read < 0
+      PortMidi.check_error LibPortMidi::PmError.new(events_read) if events_read < 0
       Array(MidiMessage).new(events_read.to_i32) do |i|
         MidiMessage.from_i32(buffer[i].message)
       end
     end
 
     def listen(callback)
+      # TODO:
+    end
+
+    def close
+      PortMidi.check_error LibPortMidi.close @stream
+    end
+  end
+
+  private class MidiInputStream < MidiStream
+    def initialize(@device_id : Int32)
+      super
+      PortMidi.check_error LibPortMidi.open_input(out @stream, @device_id, nil, 512, nil, nil)
+    end
+  end
+
+  private class MidiOutputStream < MidiStream
+    def initialize(@device_id : Int32)
+      super
+      PortMidi.check_error LibPortMidi.open_output(out @stream, @device_id, nil, 512, nil, nil, 0)
+    end
+  end
+
+  class PortMidiException < Exception
+  end
+
+  # this class is the same as the PmDeviceInfo struct
+  # with the addition of the device id for added convenience
+  private class MidiDeviceInfo
+    getter :device_id, :name, :input, :output
+
+    @input : Bool
+    @output : Bool
+
+    def initialize(device_id : Int32)
+      device_info = get_pm_midi_device_info device_id
+      @device_id = device_id
+      @name = String.new(device_info.name)
+      @input = device_info.input != 0
+      @output = device_info.output != 0
+      # @opened is not immutable
+      # hence we need a getter method which queries LibPortMidi with each access
+      # to avoid the possibility of multiple objects refering to the same device_id
+      # having a contradictory @opened instance variable between them
+    end
+
+    def opened
+      device_info = get_pm_midi_device_info @device_info
+      @opened = device_info.opened != 0
+    end
+
+    private def get_pm_midi_device_info(device_id)
+      device_info_p = LibPortMidi.get_device_info(device_id)
+      raise PortMidiException.new("Invalid device id, cannot retrieve device info") unless device_info_p
+      device_info_p.value
+    end
+
+    def to_stream
+      return MidiInputStream.new @device_id if @input
+      return MidiOutputStream.new @device_id
     end
   end
 end
 
+# turnon PortMidi
 PortMidi.start
-# get the devices
-d_in = PortMidi.get_all_midi_devices.select { |d| d.input && d.name.match /2/ }[0]
-d_out = PortMidi.get_all_midi_devices.select { |d| d.output }[0]
-# open them
-d_in.open
-d_out.open
+# get the midi streams (and open them)
+d_in = PortMidi.get_all_midi_device_info.select { |d| d.input && d.name.match /2/ }[0].to_stream
+d_out = PortMidi.get_all_midi_device_info.select { |d| d.output }[0].to_stream
 # log them
 p d_in
 p d_out
@@ -132,26 +133,8 @@ d_out.write([note_off(56), note_off(77)])
 # get midi in
 messages = d_in.read
 p messages
-p "d_out.opened: #{d_out.opened}"
 # close them
 d_in.close
 d_out.close
-p "d_out.close(); d_out.opened: #{d_out.opened}"
-#
-# d_in0 = PortMidi.get_all_midi_devices.select { |d| d.input && d.name.match /2/ }[0]
-# d_in1 = PortMidi.get_all_midi_devices.select { |d| d.input && d.name.match /2/ }[0]
-# d_out0 = PortMidi.get_all_midi_devices.select { |d| d.output }[0]
-# d_out1 = PortMidi.get_all_midi_devices.select { |d| d.output }[0]
-# # open them
-# d_in0.open
-# d_in1.open
-# d_out0.open
-# d_out1.open
-# 
-# # close them
-# d_in0.close
-# d_in1.close
-# d_out0.close
-# d_out1.close
-# 
+# turn off PortMidi
 PortMidi.stop
